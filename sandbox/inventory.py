8473b21a299582c88c65e259a1656e88f9c3d71a
Rossmm1990 <rossmmorgan@gmail.com>
2022-12-30 20:27:24 -0700

    Update inventory.py
    changes to script to get it to work with both a list and a dictionary.  Not complete

--- sandbox/inventory.py
+++ sandbox/inventory.py
@@ -6,7 +6,17 @@ def add_inv(item, item_abr):
     inventory[f"{item_abr}"] = item
 
 # fucntion open_inv to see what is in inventory and return whatevr you choose to equip
-def open_inv():
+def open_inv_list():
+    print("You open your inventory and look a the contents")
+    print("Your inventory contains...")
+    print(','.join(inventory.keys()))
+    print("Select item by typing name of item")
+    selected = input("> ")
+    in_hand = inventory[f'{selected}']
+    return in_hand
+
+# fucntion open_inv to see what is in inventory and return whatevr you choose to equip
+def open_inv_dict():
     print("You open your inventory and look a the contents")
     print("Your inventory contains...")
     print(','.join(inventory.keys()))
@@ -16,33 +26,25 @@ def open_inv():
     return in_hand
 
 # fucntion that tells you what you equipped!
-def equip_item():
-    equipped = open_inv()
+def equip_item(type):
+    equipped = type
     print(f"You equipped a {equipped}")
 
-# calls function open_inv
-open_inv()
+def main():
+    print("You come across a sword! Do you pick it up?")
 
-# calls function open equip_teim
-equip_item()
+    action = input("what would you like to do? > ")
+    
+    if action == "pick up from list":
+        equip_item(open_inv_list)
+    
+    elif action == "pick up from dict":
+        equip_item(open_inv_dict)
+        
+    else:
+        print("You do nothing")
+        main()
 
-
-
-print("You come across a sword! Do you pick it up?")
-
-pick_up = input("Type yes or no > ")
-
-# if statement for if you picked up sword if yes assigns sword to two variables then runs add_inv function using the two variables
-if pick_up == "yes":
-    a = "Sword"
-    b = "sword"
-    add_inv(a, b)
-else:
-    print("You leave the sword on the ground")
-
-open_inv()
-
-equip_item()
-
-
+main()
+ 
 
