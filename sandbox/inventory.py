4059ba8dfd4ea5734e394285aee4453b7de6d852
Rossmm1990 <rossmmorgan@gmail.com>
2023-01-02 13:24:47 -0700

    Update inventory.py
    made the script work with both a dict and a list.  Also changed up the equip function and put it inside of the inventory functions.

--- sandbox/inventory.py
+++ sandbox/inventory.py
@@ -1,5 +1,6 @@
 #created dictionary called inventory
-inventory = {"stick": "Stick"}
+inventory = {"stick": "stick", "cucumber": "cucumber"}
+inventory_lst = ["stick", "cucumber"]
 
 # function that adds item from game into the inventory dictionary
 def add_inv(item, item_abr):
@@ -9,11 +10,12 @@ def add_inv(item, item_abr):
 def open_inv_list():
     print("You open your inventory and look a the contents")
     print("Your inventory contains...")
-    print(','.join(inventory.keys()))
-    print("Select item by typing name of item")
+    print(','.join(inventory_lst))
+    print("Select item by typing number of item")
     selected = input("> ")
-    in_hand = inventory[f'{selected}']
-    return in_hand
+    in_hand = inventory_lst[(int(selected))]
+    equip_item(in_hand)
+    
 
 # fucntion open_inv to see what is in inventory and return whatevr you choose to equip
 def open_inv_dict():
@@ -23,12 +25,21 @@ def open_inv_dict():
     print("Select item by typing name of item")
     selected = input("> ")
     in_hand = inventory[f'{selected}']
-    return in_hand
+    equip_item(in_hand)
+    
 
 # fucntion that tells you what you equipped!
 def equip_item(type):
-    equipped = type
-    print(f"You equipped a {equipped}")
+    equip = input(f"Do you equip it or put the {type} away? \n>  ")
+
+    if equip == "equip it":
+        print(f"You equipped a {type}")
+        return type
+    elif equip == "put away":
+        print(f"You put away {type}")
+    else:
+        print("Your are a bugger eatin moron, try again")
+        equip_item(type)
 
 def main():
     print("You come across a sword! Do you pick it up?")
@@ -36,10 +47,10 @@ def main():
     action = input("what would you like to do? > ")
     
     if action == "pick up from list":
-        equip_item(open_inv_list)
+        open_inv_list()
     
     elif action == "pick up from dict":
-        equip_item(open_inv_dict)
+        open_inv_dict()
         
     else:
         print("You do nothing")
