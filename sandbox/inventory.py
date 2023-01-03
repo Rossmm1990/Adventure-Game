4f6f580aec6f4ac5d89f5e3f8247ecd1ffb8cd21
Rossmm1990 <rossmmorgan@gmail.com>
2023-01-02 18:01:58 -0700

    Update inventory.py
    fixed bug with randomization and changed some of the printed text

--- sandbox/inventory.py
+++ sandbox/inventory.py
@@ -7,9 +7,7 @@ inventory_list = ["stick", "cucumber"]
 
 item_list = random.choice(tuple(master_item_list))
 item_dict = random.choice(list(master_item_dict.items()))
-
-
-
+item_dict_single = item_dict[1]
 
 
 
@@ -52,23 +50,41 @@ def equip_item(type):
         print("Your are a bugger eatin moron, try again")
         equip_item(type)
 
-def main(item):
-    print(f"You come across a {item} !")
+def main(item_list, item_dict, item_dict_single):
+    print("You come across a chest !")
 
     action = input("what would you like to do? > ")
     
-    if action == "pick up from list":
-        inventory_list.append(item)
-        open_inv_list()
+    if action == "open up from list":
+        print(f"It is a {item_list}")
+        answer = input(f"Do you pick up the {item_list}?")
+        
+        if answer == "yes":
+            inventory_list.append(item_list)
+            open_inv_list()
+        elif answer == "no":
+            print("You set it down and move on")
+        else:
+            print("Try again you dummy")
+            main(item_list, item_dict, item_dict_single)
     
-    elif action == "pick up from dict":
-        inventory[item_dict[0]] = item_dict[1]
-        open_inv_dict()
+    elif action == "open up from dict":
+        print(f"It is a {item_dict_single}")
+        answer = input(f"Do you pick up the {item_dict_single}?")
+        
+        if answer == "yes":
+            inventory[item_dict[0]] = item_dict[1]
+            open_inv_dict()
+        elif answer == "no":
+            print("You set it down and move on")
+        else:
+            print("Try again you dummy")
+            main(item_list, item_dict, item_dict_single)
         
     else:
         print("You do nothing")
-        main(item, item_list)
+        main(item_list, item_dict, item_dict_single)
 
-main(item_list)
+main(item_list, item_dict, item_dict_single)
  
 
