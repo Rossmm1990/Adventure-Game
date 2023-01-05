36093a9b9b6a3349c1f9ddb194f6ea7fa6c0fa3b
Rossmm1990 <rossmmorgan@gmail.com>
2023-01-04 18:42:59 -0700

    Update inventory.py
    deleted all the list functions and changed the dict

--- sandbox/inventory.py
+++ sandbox/inventory.py
@@ -1,11 +1,16 @@
 import random
 
-master_item_list = ["sword", "axe", "shield", "spear", "bucket of poop"]
-master_item_dict = {"sword": "sword", "axe": "axe", "shield": "sheild", "spear": "spear", "bucket of poop": "bucket of poop"}
-inventory = {"stick": "stick", "cucumber": "cucumber"}
-inventory_list = ["stick", "cucumber"]
+master_item_dict = {
+    "sword": {}, 
+    "axe": {}, 
+    "shield":{}, 
+    "spear":{}, 
+    "bucket of poop":{},
+}
 
-item_list = random.choice(tuple(master_item_list))
+inventory = {"stick": "stick", "cucumber": "cucumber"}
+
+
 item_dict = random.choice(list(master_item_dict.items()))
 item_dict_single = item_dict[1]
 
@@ -14,17 +19,6 @@ item_dict_single = item_dict[1]
 # function that adds item from game into the inventory dictionary
 def add_inv(item, item_abr):
     inventory[f"{item_abr}"] = item
-
-# fucntion open_inv to see what is in inventory and return whatevr you choose to equip
-def open_inv_list():
-    print("You open your inventory and look a the contents")
-    print("Your inventory contains...")
-    print(','.join(inventory_list))
-    print("Select item by typing number of item")
-    selected = input("> ")
-    in_hand = inventory_list[(int(selected))]
-    equip_item(in_hand)
-    
 
 # fucntion open_inv to see what is in inventory and return whatevr you choose to equip
 def open_inv_dict():
@@ -50,41 +44,29 @@ def equip_item(type):
         print("Your are a bugger eatin moron, try again")
         equip_item(type)
 
-def main(item_list, item_dict, item_dict_single):
+def main(item_dict, item_dict_single):
     print("You come across a chest !")
 
     action = input("what would you like to do? > ")
     
-    if action == "open up from list":
-        print(f"It is a {item_list}")
-        answer = input(f"Do you pick up the {item_list}?")
-        
-        if answer == "yes":
-            inventory_list.append(item_list)
-            open_inv_list()
-        elif answer == "no":
-            print("You set it down and move on")
-        else:
-            print("Try again you dummy")
-            main(item_list, item_dict, item_dict_single)
-    
-    elif action == "open up from dict":
-        print(f"It is a {item_dict_single}")
-        answer = input(f"Do you pick up the {item_dict_single}?")
-        
-        if answer == "yes":
-            inventory[item_dict[0]] = item_dict[1]
-            open_inv_dict()
-        elif answer == "no":
-            print("You set it down and move on")
-        else:
-            print("Try again you dummy")
-            main(item_list, item_dict, item_dict_single)
-        
+    if action == "open up from dict":
+        print(f"You open the chest and find {item_dict_single}")
+    elif action == "nothing":
+        print("You move on and leave the chest")
     else:
-        print("You do nothing")
-        main(item_list, item_dict, item_dict_single)
+        print("Type the right choice you moron!!")
 
-main(item_list, item_dict, item_dict_single)
+    answer = input(f"Do you pick up the {item_dict_single}?")
+        
+    if answer == "yes":
+        inventory[item_dict[0]] = item_dict[1]
+        open_inv_dict()
+    elif answer == "no":
+        print("You set it down and move on")
+    else:
+        print("Try again you dummy")
+        main(item_list, item_dict, item_dict_single)
+        
+main(item_dict, item_dict_single)
  
 
