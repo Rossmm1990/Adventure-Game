102d67600f16eebd85a39575b176a5852e27468a
Rossmm1990 <rossmmorgan@gmail.com>
2023-01-11 20:40:09 -0700

    created beginning class

--- sandbox/class_test.py
+++ sandbox/class_test.py
@@ -5,7 +5,7 @@ def Intro():
     Name = input("What is your name Adventurer? \n")
     print(f"Well met {Name} what kind of skills to you bring to our raiding party? \n")
     print("""Please in the below questions put a number value for each skill that 
-    comes up.  You have a total of 20 points to distribute between VIGOR, STRENGHT, 
+    comes up.  You have a total of 20 points to distribute between VIGOR, strength, 
     INTELLECT, DEXTERITY, and LUCK. If you go over 20 points you will be asked to put
     in a different number till totaly number of points equal 20. /n""")
 
@@ -18,36 +18,36 @@ def vigor_points():
     vigor_points_int = int(vigor_points)
 
     if vigor_points_int in range(0, 20) and vigor_points_int < 20:
-        strenght_points()
-        return vigor_points_int
+        strength_points()
+        
     
     else:
       print("You used to many points! try again")
       health_points() 
 
-def strenght_points():
+def strength_points():
     vigor = vigor_points()
 
-    strenght_points = input("""Your strenght determines how much dmg you do to others.
-    How many points would you like to put in strenght? >  \n""")
+    strength_points = input("""Your strength determines how much dmg you do to others.
+    How many points would you like to put in strength? >  \n""")
 
-    strenght_points_int = int(strenght_points)
+    strength_points_int = int(strength_points)
 
-    if strenght_points_int in range(0, 20) and strenght_points_int + vigor  < 20:
+    if strength_points_int in range(0, 20) and strength_points_int + vigor_points_int  < 20:
       intellect_points()
     
     else:
       print("You used to many points! try again")
-      strenght_points()
+      strength_points()
 
 def intellect_points():
 
-    intellect_points = input("""Your strenght determines how much dmg you do to others.
-    How many points would you like to put in strenght? >  \n""")
+    intellect_points = input("""Your strength determines how much dmg you do to others.
+    How many points would you like to put in strength? >  \n""")
 
     intellect_points_int = int(intellect_points)
 
-    if intellect_points_int in range(0, 20) and strenght_points_int + vigor_points_int + intellect_points_int < 20:
+    if intellect_points_int in range(0, 20) and strength_points_int + vigor_points_int + intellect_points_int < 20:
       intellect_points()
     
     else:
@@ -57,12 +57,12 @@ def intellect_points():
 
 def dexterity_points():
 
-    dexterity_points = input("""Your strenght determines how much dmg you do to others.
-    How many points would you like to put in strenght? >  \n""")
+    dexterity_points = input("""Your strength determines how much dmg you do to others.
+    How many points would you like to put in strength? >  \n""")
 
     dexterity_points_int = int(dexterity_points)
 
-    if dexterity_points_int in range(0, 20) and strenght_points_int + vigor_points_int + intellect_points_int + dexterity_points_int < 20:
+    if dexterity_points_int in range(0, 20) and strength_points_int + vigor_points_int + intellect_points_int + dexterity_points_int < 20:
       dexterity_points()
     
     else:
@@ -71,36 +71,50 @@ def dexterity_points():
 
 def luck_points():
 
-    luck_points = input("""Your strenght determines how much dmg you do to others.
-    How many points would you like to put in strenght? >  \n""")
+    luck_points = input("""Your strength determines how much dmg you do to others.
+    How many points would you like to put in strength? >  \n""")
 
     luck_points_int = int(luck_points)
 
-    if luck_points_int in range(0, 20) and strenght_points_int + vigor_points_int + intellect_points_int + dexterity_points_int + luck_points_int < 20:
+    if luck_points_int in range(0, 20) and strength_points_int + vigor_points_int + intellect_points_int + dexterity_points_int + luck_points_int < 20:
       luck_points()
     
     else:
       print("You used to many points! try again")
       luck_points()   
 
+class beginning:
+  attribute_pool = 20
+
+  attribute_manaul = {
+    "vigor": "how much dmg you do to others",
+    "strength": "how much dmg you do to others",
+    "intellect": "how much dmg you do to others",
+    "dexterity": "how much dmg you do to others",
+    "luck": "how much dmg you do to others",
+  }
 
   
+
+  
+
 class Character:
-  def __init__(self, vigor, strenght, intellect, dexterity, luck):
-    self.vigor = vigor
-    self.strenght = strenght
+  def __init__(self, vigor, strength, intellect, dexterity, luck):
+    self.vigor = 10 + vigor
+    self.strength = strength
     self.intellect = intellect
     self.dexterity = dexterity
     self.luck = luck
-  
 
-class Character_Creation_User(Character):
 
-  def Character_Main_Stats(self):
+class Player(Character):
+  pass
 
-    Character_stats = {"vigor": self.vigor, "strenght": self.strenght, 
-    "intellect": self.intellect, "dexterity": self.dexterity, "luck": self.luck}
+Ross = Character(5, 10, 10, 10, 10)
 
-    return Character_stats
+print(Ross.vigor)
 
-Intro()
+
+
+
+
