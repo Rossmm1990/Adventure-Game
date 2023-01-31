4e2b62cb4ef6cbdd05b7588d1e711945a45f6a70
Rossmm1990 <rossmmorgan@gmail.com>
2023-01-30 19:40:12 -0700

    progress on rogue like tutorial

---
+++ jonah_examples/save_game.py
@@ -0,0 +1,20 @@
+# copy this code
+character = {
+    "name": "Jonah",
+    "health": 89,
+    "stamina": 10,
+    "stats": {
+        "strength": 10,
+        "dexterity": 10,
+        "constition": 10,
+        "intellegence": 10,
+        "wisdom": 10,
+        "charisma": 10,
+        "armor": 10,
+        "health": 50,
+    },
+    "skills": ["skill_a", "skill_b"],
+    "equipped_item": "sword",
+}
+party = ["Bobby", "Milo"]
+story = {"scene": "death_bog"}
